c7fc6bf4b46e7f653198e48e3681ec97ac67be60
Shelby Hutchinson <shhutc18@gmail.com>
2024-01-27 15:01:25 -0800

    added corrections to sql files

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,10 +16,14 @@ INSERT INTO role (title, salary, department_id) VALUES
     ('Lawyer', 190000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ('John', 'Doe', 1, 0),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, 0),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Malia', 'Brown', 5, 0),
-    ('Sarah', 'Lourd', 6, 5),
-    ('Tom', 'Allen', 7, 0);
+    ('John', 'Doe', 1, NULL),  -- John Doe has no manager
+    ('Mike', 'Chan', 2, NULL),  -- Mike Chan's manager will be John Doe, but John Doe must be inserted first
+    ('Ashley', 'Rodriguez', 3, NULL),  -- Ashley Rodriguez has no manager
+    ('Kevin', 'Tupik', 4, NULL),  -- Kevin Tupik's manager will be Ashley Rodriguez, but Ashley must be inserted first
+    ('Malia', 'Brown', 5, NULL),  -- Malia Brown has no manager
+    ('Sarah', 'Lourd', 6, NULL),  -- Sarah Lourd's manager will be Malia Brown, but Malia must be inserted first
+    ('Tom', 'Allen', 7, NULL);  -- Tom Allen has no manager
+
+UPDATE employee SET manager_id = 1 WHERE first_name = 'Mike' AND last_name = 'Chan';
+UPDATE employee SET manager_id = 3 WHERE first_name = 'Kevin' AND last_name = 'Tupik';
+UPDATE employee SET manager_id = 5 WHERE first_name = 'Sarah' AND last_name = 'Lourd';
