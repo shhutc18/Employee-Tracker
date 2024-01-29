d62a9a4af9ae6ea03b2bd1c38e2772fa7f79c81a
Shelby Hutchinson <shhutc18@gmail.com>
2024-01-28 19:21:49 -0800

    added list functionality to some functions for user accessibility

--- server.js
+++ server.js
@@ -52,38 +52,69 @@ function viewRoles() {
 }
 
 function addEmployee() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'first_name',
-            message: 'What is the employee\'s first name?'
-        },
-        {
-            type: 'input',
-            name: 'last_name',
-            message: 'What is the employee\'s last name?'
-        },
-        {
-            type: 'input',
-            name: 'role_id',
-            message: 'What is the employee\'s role ID?'
-        },
-        {
-            type: 'input',
-            name: 'manager_id',
-            message: 'What is the employee\'s manager ID?'
+    // First, fetch the current roles and managers from the database
+    db.query('SELECT * FROM role', function (err, roles) {
+        if (err) {
+            console.log(err);
+            init();
+            return;
         }
-    ]).then((answers) => {
-        db.query('INSERT INTO employee SET ?', answers, function (err, results) {
+
+        db.query('SELECT * FROM employee', function (err, managers) {
             if (err) {
                 console.log(err);
                 init();
-            } else {
-                console.log("Employee added!");
-                init();
+                return;
             }
+
+            // Then, create arrays of role titles and manager names
+            const roleChoices = roles.map(role => role.title);
+            const managerChoices = managers.map(manager => `${manager.first_name} ${manager.last_name}`);
+
+            // Now, prompt the user
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'first_name',
+                    message: 'What is the employee\'s first name?'
+                },
+                {
+                    type: 'input',
+                    name: 'last_name',
+                    message: 'What is the employee\'s last name?'
+                },
+                {
+                    type: 'list',
+                    name: 'role_id',
+                    message: 'What is the employee\'s role?',
+                    choices: roleChoices
+                },
+                {
+                    type: 'list',
+                    name: 'manager_id',
+                    message: 'Who is the employee\'s manager?',
+                    choices: managerChoices
+                }
+            ]).then((answers) => {
+                // Find the selected role's and manager's IDs
+                const role = roles.find(role => role.title === answers.role_id);
+                answers.role_id = role.id;
+
+                const manager = managers.find(manager => `${manager.first_name} ${manager.last_name}` === answers.manager_id);
+                answers.manager_id = manager.id;
+
+                db.query('INSERT INTO employee SET ?', answers, function (err, results) {
+                    if (err) {
+                        console.log(err);
+                        init();
+                    } else {
+                        console.log("Employee added!");
+                        init();
+                    }
+                });
+            });
         });
-    })
+    });
 }
 
 function addDepartment() {
@@ -107,58 +138,107 @@ function addDepartment() {
 }
 
 function addRole() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the role\'s title?'
-        },
-        {
-            type: 'input',
-            name: 'salary',
-            message: 'What is the role\'s salary?'
-        },
-        {
-            type: 'input',
-            name: 'department_id',
-            message: 'What is the role\'s department ID?'
+    // First, fetch the current departments from the database
+    db.query('SELECT * FROM department', function (err, departments) {
+        if (err) {
+            console.log(err);
+            init();
+            return;
         }
-    ]).then((answers) => {
-        db.query('INSERT INTO role SET ?', answers, function (err, results) {
-            if (err) {
-                console.log(err);
-                init();
-            } else {
-                console.log("Role added!");
-                init();
+
+        // Then, create an array of department names
+        const departmentChoices = departments.map(department => department.name);
+
+        // Now, prompt the user
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the role\'s title?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the role\'s salary?'
+            },
+            {
+                type: 'list',
+                name: 'department_id',
+                message: 'What department does the role belong to?',
+                choices: departmentChoices
             }
+        ]).then((answers) => {
+            // Find the selected department's ID
+            const department = departments.find(department => department.name === answers.department_id);
+            answers.department_id = department.id;
+
+            db.query('INSERT INTO role SET ?', answers, function (err, results) {
+                if (err) {
+                    console.log(err);
+                    init();
+                } else {
+                    console.log("Role added!");
+                    init();
+                }
+            });
         });
-    })
+    });
 }
 
 function updateRole() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is the employee\'s ID?'
-        },
-        {
-            type: 'input',
-            name: 'role_id',
-            message: 'What is the employee\'s new role ID?'
+    // First, fetch the current employees and roles from the database
+    db.query('SELECT * FROM employee', function (err, employees) {
+        if (err) {
+            console.log(err);
+            init();
+            return;
         }
-    ]).then((answers) => {
-        db.query('UPDATE employee SET role_id = ? WHERE id = ?', [answers.role_id, answers.id], function (err, results) {
+
+        db.query('SELECT * FROM role', function (err, roles) {
             if (err) {
                 console.log(err);
                 init();
-            } else {
-                console.log("Employee role updated!");
-                init();
+                return;
             }
+
+            // Then, create arrays of employee names and role titles
+            const employeeChoices = employees.map(employee => `${employee.first_name} ${employee.last_name}`);
+            const roleChoices = roles.map(role => role.title);
+
+            // Now, prompt the user
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'id',
+                    message: 'Which employee\'s role do you want to update?',
+                    choices: employeeChoices
+                },
+                {
+                    type: 'list',
+                    name: 'role_id',
+                    message: 'What is the employee\'s new role?',
+                    choices: roleChoices
+                }
+            ]).then((answers) => {
+                // Find the selected employee's and role's IDs
+                const employee = employees.find(employee => `${employee.first_name} ${employee.last_name}` === answers.id);
+                answers.id = employee.id;
+
+                const role = roles.find(role => role.title === answers.role_id);
+                answers.role_id = role.id;
+
+                db.query('UPDATE employee SET role_id = ? WHERE id = ?', [answers.role_id, answers.id], function (err, results) {
+                    if (err) {
+                        console.log(err);
+                        init();
+                    } else {
+                        console.log("Employee role updated!");
+                        init();
+                    }
+                });
+            });
         });
-    })
+    });
 }
 
 function init () {
